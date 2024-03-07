5fdebfdc00d5092879450f457da4d77a00f6c416
MathiMomos <mathi.gamer.m@gmail.com>
2024-03-07 09:10:34 -0500

    Momos

--- src/main/java/net/mathimomos/erythrium/Erythrium.java
+++ src/main/java/net/mathimomos/erythrium/Erythrium.java
@@ -1,6 +1,9 @@
 package net.mathimomos.erythrium;
 
 import com.mojang.logging.LogUtils;
+import net.mathimomos.erythrium.item.ModCreativeModeTabs;
+import net.mathimomos.erythrium.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -16,13 +19,15 @@ import org.slf4j.Logger;
 
 @Mod(Erythrium.MOD_ID)
 public class Erythrium {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "erythrium";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public Erythrium() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -34,9 +39,11 @@ public class Erythrium {
 
     }
 
-    // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ERYTHRIUM);
+            event.accept(ModItems.ROUGH_ERYTHRIUM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/mathimomos/erythrium/block/ModBlocks.java
@@ -0,0 +1,4 @@
+package net.mathimomos.erythrium.block;
+
+public class ModBlocks {
+}
