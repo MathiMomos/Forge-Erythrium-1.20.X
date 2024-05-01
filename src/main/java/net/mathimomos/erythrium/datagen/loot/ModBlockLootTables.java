b3c7e733f0c6118d900d176a0508bcaa0edfa28d
MathiMomos <mathi.gamer.m@gmail.com>
2024-05-01 18:40:17 -0500

    DATAGEN FINISHED

--- src/main/java/net/mathimomos/erythrium/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/mathimomos/erythrium/datagen/loot/ModBlockLootTables.java
@@ -6,7 +6,15 @@ import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer;
+import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Set;
@@ -25,7 +33,22 @@ public class ModBlockLootTables extends BlockLootSubProvider {
 
         this.add(ModBlocks.ERYTHRIUM_ORE.get(),
                 block -> createOreDrop(ModBlocks.ERYTHRIUM_ORE.get(), ModItems.ERYTHRIUM.get()));
+        this.add(ModBlocks.DEEPSLATE_ERYTHRIUM_ORE.get(),
+                block -> createOreDrop(ModBlocks.DEEPSLATE_ERYTHRIUM_ORE.get(), ModItems.ERYTHRIUM.get()));
+        this.add(ModBlocks.DEEPSLATE_ERYTHRIUM_ORE.get(),
+                block -> createOreDrop(ModBlocks.DEEPSLATE_ERYTHRIUM_ORE.get(), ModItems.ERYTHRIUM.get()));
+        this.add(ModBlocks.COMPACTED_ERYTHRIUM.get(),
+                block -> createErythriumOreDrops(ModBlocks.COMPACTED_ERYTHRIUM.get(), ModItems.ERYTHRIUM.get()));
     }
+
+    protected LootTable.Builder createErythriumOreDrops(Block pBlock, Item pItem) {
+        return createSilkTouchDispatchTable(pBlock,
+                this.applyExplosionDecay(pBlock,
+                LootItem.lootTableItem(pItem)
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(3.0F, 4.0F)))
+                        .apply(ApplyBonusCount.addOreBonusCount(Enchantments.BLOCK_FORTUNE))));
+    }
+
 
     @Override
     protected Iterable<Block> getKnownBlocks() {
